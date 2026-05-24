2b4be95551b9b80ba286fbcfe0ee177800f21bad
DoomTay <9124594+DoomTay@users.noreply.github.com>
2026-05-24 12:25:23 -0400

    Refactored to wean code from literal ASM logic

--- soscodec.py
+++ soscodec.py
@@ -5,15 +5,13 @@ codec_step_table = [7, 8, 9, 10, 11, 12, 13, 14, 16,  17,  19,  21,  23,  25,  2
 compression_info = {
     "source": 0,
     "dest": 0,
-    "comp_size": 0,
     "sample_index": 0,
     "predicted": 0,
     "difference": 0,
     "code_buffer": 0,
     "code": 0,
     "step": 0,
-    "index": 0,
-    "bit_size": 0
+    "index": 0
 }
 
 codec_bytes_processed = 0
@@ -31,170 +29,97 @@ def sos_codec_decompress_data(audio_bytes):
     global codec_bytes_processed, codec_byte_index, compression_info
 
     def decomp_main_loop():
-        nonlocal eax
         if (compression_info["sample_index"] & 1) == 0:
             decomp_fetch_token()
         else:
-            eax = compression_info["code_buffer"] & 0xffff
-            eax = (eax >> 4) & 0xf
+            new_buffer = compression_info["code_buffer"] & 0xffff
+            new_buffer = (new_buffer >> 4) & 0xf
             
-            ax = eax & 0xffff
-            compression_info["code"] = ax
-                        
+            compression_info["code"] = new_buffer & 0xffff
+            
             decomp_calc_difference()
             
     def decomp_fetch_token():
-        nonlocal esi, eax, audio_bytes
-        eax = 0
-        al = audio_bytes[esi] & 0xff
-        eax = al
-        ax = eax & 0xffff
-        compression_info["code_buffer"] = ax
-        esi += 1
-        eax &= 0x000f
-        ax = eax & 0xffff
-        compression_info["code"] = ax
+        nonlocal source_index, audio_bytes
+        compressed_byte = audio_bytes[source_index] & 0xff
+        compression_info["code_buffer"] = compressed_byte & 0xffff
+        source_index += 1
+        token = compressed_byte & 0x000f
+        compression_info["code"] = token & 0xffff
         
         decomp_calc_difference()
         
-        
     def decomp_calc_difference():
-        nonlocal eax,ecx
         compression_info["difference"] = 0
-        ecx = compression_info["step"] & 0xffff
-        
-        if (eax & 4) == 0:
-            decomp_no_4()
-        else:
-            compression_info["difference"] += ecx
-            decomp_no_4()
-            
-    def decomp_no_4():
-        nonlocal eax,ecx,edx
-        if (eax & 2) == 0:
-            decomp_no_2()
-        else:
-            edx = ecx
-            edx = edx >> 1
-            compression_info["difference"] += edx
-            decomp_no_2()
-            
-    def decomp_no_2():
-        nonlocal eax,ecx,edx
-        if (eax & 1) == 0:
-            decomp_no_1()
-        else:
-            edx = ecx
-            edx = edx >> 2
-            compression_info["difference"] += edx
-            decomp_no_1()
-            
-    def decomp_no_1():
-        nonlocal eax, ecx, edx
-        edx = ecx
-        edx = edx >> 3
-        compression_info["difference"] += edx
-        
-        if (compression_info["code"] & 8) == 0:
-            decomp_no_8()
-        else:
-            compression_info["difference"] = -compression_info["difference"]
-            decomp_no_8()
-            
-    def decomp_no_8():    
-        nonlocal eax
-        eax = compression_info["predicted"]
-        eax += compression_info["difference"]
-        
-        # was 0x7fff
-        if(eax <= 32767):
-            decomp_no_overflow()
-        else:
-            eax = 32767
-            decomp_no_overflow()
-            
-    def decomp_no_overflow():
-        nonlocal eax
-        # was 0xffff8000
-        if(eax >= -32768):
-            decomp_no_underflow()
-        else:
-            eax = -32768
-            decomp_no_underflow()
-            
+        step = compression_info["step"] & 0xffff
+
+        if (compression_info["code"] & 4) != 0: compression_info["difference"] += step
+        if (compression_info["code"] & 2) != 0: compression_info["difference"] += step >> 1
+        if (compression_info["code"] & 1) != 0: compression_info["difference"] += step >> 2
+
+        compression_info["difference"] += step >> 3
+
+        if (compression_info["code"] & 8) != 0: compression_info["difference"] = -compression_info["difference"]
+
+        clamp_predicted()
+
+    def clamp_predicted():
+        compression_info["predicted"] += compression_info["difference"]
+
+        compression_info["predicted"] = max(-32768, min(compression_info["predicted"], 32767))
+
+        decomp_no_underflow()
+
     def decomp_no_underflow():
-        nonlocal edi, converted_bytes, eax, ecx
-        compression_info["predicted"] = eax
-                
-        ax = eax & 0xFFFF
-        ah = (ax >> 8) & 0xff
-        ah ^= 0x80
-        al = ah
+        nonlocal dest_index, converted_bytes
+
+        predicted_high_byte = compression_info["predicted"] & 0xFFFF
         
-        converted_bytes[edi] = al
-        edi += 1
+        converted_bytes[dest_index] = ((predicted_high_byte >> 8) & 0xff) ^ 0x80
+        dest_index += 1
         
-        ecx = compression_info["code"]
-        eax = codec_index_table[ecx]
-        ax = eax & 0xFFFF
-        compression_info["index"] += ax
+        code_index = compression_info["code"]
+        lookup_value = codec_index_table[code_index] & 0xFFFF
+        compression_info["index"] += lookup_value
         
         compression_info["index"] = compression_info["index"] & 0xFFFF
-       
+        
         if compression_info["index"] < 0x8000:
-            decomp_check_overflow()
-        else:
-            compression_info["index"] = 0
-            decomp_adjust_step()
+            compression_info["index"] = compression_info["index"] & 0xFFFF
+            if compression_info["index"] > 88: compression_info["index"] = 88
+        else: compression_info["index"] = 0
             
-    def decomp_check_overflow():
-        compression_info["index"] = compression_info["index"] & 0xFFFF
-        
-        if compression_info["index"] <= 88:
-            decomp_adjust_step()
-        else:
-            compression_info["index"] = 88
-            decomp_adjust_step()
+        decomp_adjust_step()
         
     def decomp_adjust_step():
         global codec_byte_index,codec_step_table
-        nonlocal esi, edi, eax, ecx
-        ecx = compression_info["index"] & 0xffff
-        eax = codec_step_table[ecx] & 0xffff
-        
-        ax = eax & 0xFFFF
+        nonlocal source_index, dest_index
+        new_index = compression_info["index"] & 0xffff
+        new_step = codec_step_table[new_index] & 0xffff
         
         compression_info["sample_index"] += 1
-        compression_info["step"] = ax
+        compression_info["step"] = new_step & 0xFFFF
         
         codec_byte_index -= 1
         if codec_byte_index == 0:
-            compression_info["source"] = esi
-            compression_info["dest"] = edi
+            compression_info["source"] = source_index
+            compression_info["dest"] = dest_index
             
-            eax = codec_bytes_processed
+            new_step = codec_bytes_processed
             
     
     sos_codec_init_stream()
     
-    eax = compression_info
-    edx = len(audio_bytes)
+    compressed_length = len(audio_bytes)
     
-    ebx = 0
-    ecx = 0
-    
-    ebx = eax
-    
-    eax = edx
-    
-    codec_bytes_processed = eax
-    codec_byte_index = eax * 2
-    
-    esi = compression_info["source"]
-    edi = compression_info["dest"]
-    
-    converted_bytes = bytearray(edx * 2)
-    
+    codec_bytes_processed = compressed_length
+    codec_byte_index = compressed_length * 2
+
+    source_index = compression_info["source"]
+    dest_index = compression_info["dest"]
+
+    converted_bytes = bytearray(compressed_length * 2)
+
     while codec_byte_index > 0:
         decomp_main_loop()
     
